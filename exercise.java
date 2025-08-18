e70f7e79eac846e8dc6ce486a025d6b2454db715
sivadhanush726 <101310244+sivadhanush726@users.noreply.github.com>
2025-08-18 05:16:41 -0700

    Add files via upload

---
+++ exercise.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa;
+
+/**
+ *
+ * @author A
+ */
+public class exercise 
+{
+    public static void main(String[] args) 
+    {
+        myclass obj=new myclass();
+        obj.add();
+        
+    }
+    
+}
